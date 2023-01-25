d7189c88310a565e4bf4e6e52effe78d836e4eeb
Kabelo Mighty Hlungwani <53508373+kabelo-mighty@users.noreply.github.com>
2023-01-25 10:15:30 +0200

    services added

--- BACKEND/FRONTEND/app/src/app/app.module.ts
+++ BACKEND/FRONTEND/app/src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgToastModule } from 'ng-angular-popup';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { PDashboardComponent } from './passenger/p-dashboard/p-dashboard.component';
+import { PProfileComponent } from './passenger/p-profile/p-profile.component';
+import { PPaymentComponent } from './passenger/p-payment/p-payment.component';
+import { AuthService } from './service/auth.service';
+import { GuardService } from './service/guard.service';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomepageComponent,
+    PDashboardComponent,
+    PProfileComponent,
+    PPaymentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,HttpClientModule,ReactiveFormsModule,NgToastModule
   ],
-  providers: [],
+  providers: [GuardService,AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
