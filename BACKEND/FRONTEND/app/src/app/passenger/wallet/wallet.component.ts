259c3b864abc42caa57e31ffe94c054353001c0b
bekidube <bekinature@gmail.com>
2023-01-27 13:57:36 +0200

    wallet done

--- BACKEND/FRONTEND/app/src/app/passenger/wallet/wallet.component.ts
+++ BACKEND/FRONTEND/app/src/app/passenger/wallet/wallet.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { JwtService } from 'src/app/service/jwt.service';
+import { PassengerService } from 'src/app/service/passenger.service';
 
 @Component({
   selector: 'app-wallet',
@@ -7,11 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WalletComponent implements OnInit {
 
-  constructor() { }
+  constructor(private jwtService : JwtService,private Passenger:PassengerService) { }
+
+  init:any
+  single:any
+  wallet:any
+
+
+  user = {
+    id: '',
+    fullname:'',
+    email:'',
+    amount:''
+  }
 
   ngOnInit(): void {
 
-
+    this.user= this.jwtService.getDetails(localStorage.getItem('token')).data.rows[0];
+    this.init = this.user.fullname.charAt(0).toUpperCase();   //display one character of fullname
+    this.single=this.user.fullname.split(' ').at(0);   //find space on fullname
+    let id=this.user.id
     
   }
 
