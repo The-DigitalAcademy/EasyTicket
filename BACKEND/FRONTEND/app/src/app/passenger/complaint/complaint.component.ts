e961a8475c9b1634868f057a9489884a35ae1893
mapharifilter <mapharifilter@gmail.com>
2023-02-13 14:58:46 +0200

    post

--- BACKEND/FRONTEND/app/src/app/passenger/complaint/complaint.component.ts
+++ BACKEND/FRONTEND/app/src/app/passenger/complaint/complaint.component.ts
@@ -1,4 +1,10 @@
+
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ThisReceiver } from '@angular/compiler';
+import { ComplaintService } from 'src/app/service/complaint.service';
+import { text } from 'stream/consumers';
+import { FormControl,FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-complaint',
@@ -7,9 +13,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComplaintComponent implements OnInit {
 
-  
-  constructor() { }
+  form:FormGroup= new FormGroup(
+  {
+    compName: new FormControl(''),
+  }
+)
 
+  constructor(private http:HttpClient,private complaintService:ComplaintService) { }
+
+    onComplaintCreate(data:any)
+  {
+
+console.log(data);
+ this.http.post('http://localhost:3001/complaint',data).subscribe((res)=>
+ {
+
+  console.log(res);
+ });
+
+  }
   ngOnInit(): void {
   }
 
